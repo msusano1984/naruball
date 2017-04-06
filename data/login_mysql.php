e85a526fc3d561ce08eaafcae053b7d4b0a28df4
Caro <carolina.moctezumav@gmail.com>
2017-04-05 23:45:57 -0500

    odionopoderusarespaciosysignosdepuntuacionjaja

--- data/login_mysql.php
+++ data/login_mysql.php
@@ -8,7 +8,7 @@ define("CONTRASENA", "123456");
 */
 
 define("NOMBRE_HOST", "localhost");// Nombre del host
-define("BASE_DE_DATOS", "subastas_db"); // Nombre de la base de modelos
-define("USUARIO", "subastas_usr"); // Nombre del usuario
-define("CONTRASENA", "sub4st4s_m41n*"); // Constraseña
+define("BASE_DE_DATOS", "naruball"); // Nombre de la base de modelos
+define("USUARIO", "naruball"); // Nombre del usuario
+define("CONTRASENA", "naruballs"); // Constraseña
 ?>
